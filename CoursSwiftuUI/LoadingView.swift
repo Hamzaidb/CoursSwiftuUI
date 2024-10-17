4695d0565077ef09043347589e755c15e9cecfe1
Hamza_Idb <hamza.idblhy@gmail.com>
2024-10-17 16:59:25 +0200

    maj

--- CoursSwiftuUI/LoadingView.swift
+++ CoursSwiftuUI/LoadingView.swift
@@ -1,18 +1,18 @@
 //
-//  MorpionView.swift
+//  LoadingView.swift
 //  CoursSwiftuUI
 //
-//  Created by IDBOUIGUIGUANE Hamza on 16/10/2024.
+//  Created by IDBOUIGUIGUANE Hamza on 17/10/2024.
 //
 
 import SwiftUI
 
-struct MorpionView: View {
+struct LoadingView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    MorpionView()
+    LoadingView()
 }
--- CoursSwiftuUI/MorpionModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  MorpionModel.swift
-//  CoursSwiftuUI
-//
-//  Created by IDBOUIGUIGUANE Hamza on 16/10/2024.
-//
-
-import Foundation
